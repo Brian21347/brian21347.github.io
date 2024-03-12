863c2c53a4318c25a1e2f3477273edd00b398093
Brian21347 <brianzhou1102@gmail.com>
2024-03-12 15:54:46 -0400

    chore: :green_heart: Update the deploy next js page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    basePath: "/brian21347.github.io",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
