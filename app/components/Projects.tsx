85c3c1b5ef4db8e7aca6583db4acd3dbfdc67616
Brian21347 <brianzhou1102@gmail.com>
2024-04-04 19:23:46 -0400

    feat(projects): Add projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,25 @@
-export default function Projects () {
-   return (
-       <div id="projects" className="bg-slate-0 text-text-0 pt-[62px]">
-           <p>Projects</p>
-       </div>
-   );
+import { ENG_projects } from "@/constants/EnglishPage";
+import { Project } from "@/constants/Types";
+
+export default function Projects() {
+  return (
+    <div id="projects" className="bg-slate-0 text-text-0 pt-[62px]">
+      <div className="relative p-5 sm:w-[75%] lg:w-[50%] m-auto rounded-xl">
+        <div className="flex items-center mb-10">
+          <div className="flex-1 border-t-2 border-slate-3 border-dotted"></div>
+          <span className="px-3 text-text-0 text-m">Projects</span>
+          <div className="flex-1 border-t-2 border-slate-3 border-dotted"></div>
+        </div>
+        {ENG_projects.map((project: Project) => (
+          <div key={project.name}>
+            <p className="text-centered font-semibold text-xl">
+              {project.name}
+            </p>
+            <project.description />
+            <br/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/constants/Types.tsx
+++ app/constants/Types.tsx
@@ -1,8 +1,15 @@
-type Role = { title: string; time: string; description: string };
-type Activity = {
+import React, { FC } from 'react';
+
+export type Role = { title: string; time: string; description: string };
+export type Activity = {
   imgSrc: string;
   imgAlt: string;
   eventName: string;
   eventLink: string;
   roles: Array<Role>;
 };
+export type Project = {
+    name: string,
+    videoSrc: string;
+    description: FC;
+}
