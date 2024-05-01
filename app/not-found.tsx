2a769f315556f50c659f182c04c2b32d35d8b24f
Brian21347 <brianzhou1102@gmail.com>
2024-05-01 19:54:17 -0400

    fix: Center text in the not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link'
- 
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="text-center">
+      <h2 className="text-xl pt-10">Sorry, but the requested page was not found</h2>
+      <Link href="/" className="text-text-0 hover:text-sky-4">
+        Return home
+      </Link>
     </div>
-  )
+  );
 }
