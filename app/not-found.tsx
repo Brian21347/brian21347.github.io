de3fdbe29484bbc4c4338892f8559ec1c0a96615
Brian21347 <brianzhou1102@gmail.com>
2024-05-01 20:16:38 -0400

    feat: Change button on not-found page to return to the previous page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,26 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div className="text-center">
-      <h2 className="text-xl pt-10">Sorry, but the requested page was not found</h2>
-      <Link href="/" className="text-text-0 hover:text-sky-4">
-        Return home
-      </Link>
+      <h2 className="text-xl pt-10">
+        Sorry, but the requested page was not found
+      </h2>
+      <button
+        onClick={() =>
+          window.history.length > 1 &&
+          document.referrer.indexOf(window.location.host) !== -1
+            ? router.back()
+            : router.replace("/")
+        }
+        className="text-text-0 hover:text-sky-4 hover:bg-slate-2 px-3 py-2 rounded-lg"
+      >
+        Go back a page
+      </button>
     </div>
   );
 }
