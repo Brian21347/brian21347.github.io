f8ec4f267b6dd1546bdf7c17b4229b5cb268124c
Brian21347 <brianzhou1102@gmail.com>
2024-06-02 22:00:23 -0400

    fix: Update wording

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="flex-col text-slate-600 font-semibold text-center">
         <h1 className="text-3xl mt-10 text-text-0">Hi! I am Brian Zhou</h1>
         <p className="text-lg mt-2 text-text-1">
-          I am a sophomore at GSMST and I am passionate about programming.
+          I am a rising junior at GSMST and I am passionate about programming.
         </p>
       </div>
     </div>
