5ee44faff412fd1342f9d90318a1888aebf64642
Brian21347 <brianzhou1102@gmail.com>
2024-09-25 21:16:37 -0400

    Update about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,10 +3,11 @@
 export default function About() {
   return (
     <div id="about" className="p-20 min-w-20 max-w-m bg-slate-0 h-max">
-      <div className="flex-col text-slate-600 font-semibold text-center">
+      <div className="flex-col text-slate-600 font-semibold text-center sm:w-[75%] lg:w-[50%] m-auto">
         <h1 className="text-3xl mt-10 text-text-0">Hi! I am Brian Zhou</h1>
         <p className="text-lg mt-2 text-text-1">
-          I am a rising junior at GSMST and I am passionate about programming.
+          I am a highly motivated junior at GSMST and a passionate programmer,
+          glad to meet you!
         </p>
       </div>
     </div>
